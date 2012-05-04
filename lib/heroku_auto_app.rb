f26d1d338cf06f94e59a38ed0245a4a715b31812
tomykaira <tomykaira@gmail.com>
2012-05-04 09:31:00 +0900

    fix paren

--- lib/heroku_auto_app.rb
+++ lib/heroku_auto_app.rb
@@ -1,11 +1,12 @@
 module Heroku::Command
   class Base
     alias_method :original_extract_app_in_dir, :extract_app_in_dir
+
     def extract_app_in_dir(dir)
       begin
         original_extract_app_in_dir(dir)
       rescue Heroku::Command::CommandFailed => e
-        if remotes = git_remotes(dir) && upstream = extract_current_upstream
+        if (remotes = git_remotes(dir)) && (upstream = extract_current_upstream)
           remotes[upstream]
         else
           raise e
